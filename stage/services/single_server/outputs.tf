b097059a64e911730eda77f4d01eb009c9884938
Htunn <martin.mystery9@gmail.com>
2020-12-10 17:03:28 +0630

    first iac_env commit

---
+++ stage/services/single_server/outputs.tf
@@ -0,0 +1,9 @@
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
+}
+
+output "alb_dns_name" {
+    value = module.webserver_cluster.alb_dns_name
+    description = "The domain name of the load balancer"
+}
